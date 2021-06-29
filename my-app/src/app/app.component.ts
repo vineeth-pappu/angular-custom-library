ffc338de9a5a9165d5994128f7a2b3612399ca41
Vineeth Viswanathan <vineeth0394@gmail.com>
2021-06-29 09:10:42 +0000

    Library working in app

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MyLibComponent} from 'my-lib'
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app';
+}
